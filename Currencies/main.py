40492a76012dc6d895fc92c7a40c36416680dad0
Netanel <njamilis@fi.uba.ar>
2020-09-12 11:22:35 -0300

    Update main.py

--- Currencies/main.py
+++ Currencies/main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import hashlib
 
 # Conexion a la base de datos
 conn = sqlite3.connect(':memory:')
@@ -10,12 +11,15 @@ cursor = conn.cursor()
 cursor.execute("""CREATE TABLE currency
                     (ID integer primary key, name text, symbol text)""")
 
+# Guardo los cambios
+conn.commit()
+
 # Inserto datos de monedas
 cursor.execute("INSERT INTO currency VALUES(1, 'Peso (ARG)', '$')")
 cursor.execute("INSERT INTO currency VALUES(2, 'Dólar', 'U$S')")
 
-# Guardo los cambios
-conn.commit()
+# Revierto los cambios
+conn.rollback()
 
 # Consulto todas las monedas
 query = "SELECT * FROM currency"
@@ -26,4 +30,38 @@ currencies = cursor.execute(query).fetchall()
 print(currencies)
 
 # Cierro la conexion con la base de datos
+conn.close()
+
+# Crear función
+def md5sum(t):
+    return hashlib.md5(t).hexdigest()
+
+
+conn = sqlite3.connect(':memory:')
+conn.create_function("md5", 1, md5sum)
+cursor = conn.cursor()
+cursor.execute("select md5(?)", (b"foo",))
+print(cursor.fetchone()[0])
+conn.close()
+
+class MySum:
+    def __init__(self):
+        self.count = 0
+
+    def step(self, value):
+        self.count += value
+    
+    def finalize(self):
+        return self.count
+
+
+conn = sqlite3.connect(':memory:')
+conn.create_function("mysum", 1, MySum)
+# sqlite3.enable_callback_tracebacks(True) 
+cursor = conn.cursor()
+cursor.execute("create table test(i)")
+cursor.execute("insert into test(i) values (1)")
+cursor.execute("insert into test(i) values (2)")
+cursor.execute("select mysum(i) from test")
+print(cursor.fetchone()[0])
 conn.close()
