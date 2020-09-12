16ae886e9b8ec774d29639043b44c19c6af5b785
Netanel <njamilis@fi.uba.ar>
2020-09-12 10:55:58 -0300

    Currencies example

---
+++ Currencies/main.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# Conexion a la base de datos
+conn = sqlite3.connect(':memory:')
+
+# Cursor
+cursor = conn.cursor()
+
+# Creo la tabla
+cursor.execute("""CREATE TABLE currency
+                    (ID integer primary key, name text, symbol text)""")
+
+# Inserto datos de monedas
+cursor.execute("INSERT INTO currency VALUES(1, 'Peso (ARG)', '$')")
+cursor.execute("INSERT INTO currency VALUES(2, 'Dólar', 'U$S')")
+
+# Guardo los cambios
+conn.commit()
+
+# Consulto todas las monedas
+query = "SELECT * FROM currency"
+
+# Busco el resultado
+currencies = cursor.execute(query).fetchall()
+
+print(currencies)
+
+# Cierro la conexion con la base de datos
+conn.close()
